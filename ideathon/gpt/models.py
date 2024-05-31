f614949d5d03874b3bba5300bca23ef5e92fa182
ryu <ryudonghyun4918@naver.com>
2024-06-01 02:22:39 +0900

    feat:models_setting_file

--- ideathon/gpt/models.py
+++ ideathon/gpt/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class GptAnswer(models.Model):
+    content = models.TextField()  
+
+    def __str__(self):
+        return self.content  
+
+class Character(models.Model):
+    character_name = models.CharField(max_length=100)
+    tone = models.CharField(max_length=100)
+    text_length = models.IntegerField()
+    situation = models.CharField()
+    language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.character_name
--- ideathon/gpt/views.py
+++ ideathon/gpt/views.py
@@ -1,3 +1,32 @@
+# gpt/views.py
 from django.shortcuts import render
+from .models import GptAnswer, Character
 
-# Create your views here.
+def gpt_answer_list(request):
+    
+    answers = GptAnswer.objects.all()
+    
+    return render(request, 'gpt/answer_list.html', {'answers': answers})
+
+def send_to_gpt(request):
+    if request.method == 'POST':
+       
+        character_name = request.POST.get('character_name')
+        tone = request.POST.get('tone')
+        text_length = request.POST.get('text_length')
+        situation = request.POST.get('situation')
+        language = request.POST.get('language')
+        
+        
+        new_character = Character.objects.create(
+            character_name=character_name,
+            tone=tone,
+            text_length=text_length,
+            situation=situation,
+            language=language
+        )
+
+        
+        return render(request, 'gpt/send_to_gpt.html', {'new_character': new_character})
+
+    return render(request, 'gpt/send_to_gpt_form.html')
